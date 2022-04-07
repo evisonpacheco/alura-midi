ee8c3a1c0b2516a3804905c91479eb5732eca0f5
Ev1son <evison.pacheco21@gmail.com>
2022-04-07 13:42:54 -0300

    update main.js: while adicionado

--- main.js
+++ main.js
@@ -2,4 +2,14 @@ function playSongPom(){
   document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick=playSongPom;
+const keyList = document.querySelectorAll('.tecla');
+
+let counter = 0;
+
+while (counter < keyList.length) {
+  keyList[counter].onclick=playSongPom;
+
+  counter = counter + 1;
+
+  console.log(counter);
+}
